03696aa6b27490655d504d74244d8e790282d42b
Mikko Rasa <tdb@tdb.fi>
2016-06-07 17:36:35 +0300

    Extensive rewrite of reach40_fast.py
    Added heuristic for permuting the found combinations if there are too
    many leftovers.  Now includes a test mode as well.

--- reach40_fast.py
+++ reach40_fast.py
@@ -1,40 +1,98 @@
 #!/usr/bin/python
 
-import sys
-q = map(int, sys.argv[1:])
-q = sorted(q, reverse=True)
-while sum(q)>=40:
-	c = [0]
-	while c:
-		s = sum(q[i] for i in c)
-		if s==40:
+def find_valid(q, b, s, t):
+	for i in range(b, len(q)):
+		if s+q[i]==t or s+q[i]+q[-1]<=t:
+			return i
+
+def increment(q, c):
+	n = q[c[-1]]
+	for i in range(c[-1]+1, len(q)):
+		if q[i]!=n:
+			c[-1] = i
+			return True
+	return False
+
+def greedy(q, t):
+	r = []
+	while sum(q)>=t:
+		c = [0]
+		while c:
+			s = sum(q[i] for i in c)
+			if s==t:
+				break
+
+			i = find_valid(q, c[-1]+1, s, t)
+			if i:
+				c.append(i)
+				continue
+
+			while c:
+				if increment(q, c):
+					break
+
+				c.pop()
+
+		if not c:
 			break
 
-		f = False
-		for i in range(c[-1]+1, len(q)):
-			if s+q[i]==40 or s+q[i]+q[-1]<=40:
-				c.append(i)
-				f = True
-				break
-		if f:
-			continue
+		r.append([q[i] for i in c])
+		q = [q[i] for i in range(len(q)) if i not in c]
 
-		while c:
-			n = q[c[-1]]
-			for i in range(c[-1]+1, len(q)):
-				if q[i]!=n:
-					c[-1] = i
-					f = True
-					break
-			if f:
-				break
+	return r, q
 
-			c.pop()
+def pair_sums(q):
+	return [(i, j, q[i]+q[j]) for i in range(0, len(q)-1) for j in range(i+1, len(q))]
 
-	if not c:
-		break
+def permute(r, q):
+	p = pair_sums(q)
+	for c in r:
+		t = pair_sums(c)
 
-	print("Found 40% combination: {}".format(" ".join([str(q[i]) for i in c])))
-	q = [q[i] for i in range(len(q)) if i not in c]
+		for x in t:
+			for y in p:
+				if x[2]==y[2] and q[y[0]]<c[x[0]]:
+					c[x[0]], q[y[0]] = q[y[0]], c[x[0]]
+					c[x[1]], q[y[1]] = q[y[1]], c[x[1]]
+					return True
 
-print("Leftovers: {}".format(" ".join(str(n) for n in q)))
+	return False
+
+def main(q, v=False):
+	q = sorted(q, reverse=True)
+
+	if v:
+		print("Input: {}".format(" ".join(str(n) for n in q)))
+
+	r, q = greedy(q, 40)
+	if v:
+		print("Leftovers after initial round: {} (total {})".format(" ".join(str(n) for n in q), sum(q)))
+	i = 0
+	while sum(q)>40 and i<100:
+		permute(r, q)
+		i += 1
+		n, q = greedy(q, 40)
+		r += n
+	for c in r:
+		print("Found 40% combination: {}".format(" ".join([str(n) for n in c])))
+	if q:
+		print("Leftovers: {} (total {})".format(" ".join(str(n) for n in q), sum(q)))
+	else:
+		print("No leftovers")
+
+def test():
+	import random
+	a = 5**0.5
+	b = 20**0.5
+	for k in range(100):
+		q = [int((a+random.random()*(b-a))**2) for i in range(100)]
+		if k:
+			print("---")
+		main(q, True)
+
+if __name__=="__main__":
+	import sys
+	if sys.argv[1]=="--test":
+		test()
+	else:
+		main(map(int, sys.argv[1:]))
